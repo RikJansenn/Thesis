19384910057ad23909eeebc81992ac0d86e940b9
RikJansenn <rik.jansen200@gmail.com>
2025-11-20 10:07:31 +0100

    parameter search for IP

--- plot_parameters.py
+++ plot_parameters.py
@@ -7,27 +7,27 @@ from scipy.stats import spearmanr
 matplotlib.use("TkAgg")
 
 def plot_parameters_against_measure(measure):
-    # fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
-    #
-    # sns.boxplot(data=data, x="N",  y=measure, ax=axes[0])
-    # sns.stripplot(data=data, x="N",  y=measure, ax=axes[0], color="black", alpha=0.5, jitter=True)
-    # axes[0].set_title(f"{measure}against number of neurons")
-    #
-    # sns.boxplot(data=data, x="sr", y=measure, ax=axes[1])
-    # sns.stripplot(data=data, x="sr", y=measure, ax=axes[1], color="black", alpha=0.5, jitter=True)
-    # axes[1].set_title(f"{measure} against spectral radius")
-    #
-    # sns.boxplot(data=data, x="lr", y=measure, ax=axes[2])
-    # sns.stripplot(data=data, x="lr", y=measure, ax=axes[2], color="black", alpha=0.5, jitter=True)
-    # axes[2].set_title(f"{measure} against leaky rate")
-    #
-    # plt.tight_layout()
-    # plt.savefig(f"{measure}_vs_parameters")
-    # plt.show()
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
 
-    sns.pairplot(data, x_vars=["N", "sr", "lr"], y_vars=measure, kind="reg", height=4)
-    plt.savefig(f"plots/{measure}_vs_parameters_v2")
+    sns.boxplot(data=data, x="N",  y=measure, ax=axes[0])
+    sns.stripplot(data=data, x="N",  y=measure, ax=axes[0], color="black", alpha=0.5, jitter=True)
+    axes[0].set_title(f"{measure}against number of neurons")
+
+    sns.boxplot(data=data, x="sr", y=measure, ax=axes[1])
+    sns.stripplot(data=data, x="sr", y=measure, ax=axes[1], color="black", alpha=0.5, jitter=True)
+    axes[1].set_title(f"{measure} against spectral radius")
+
+    sns.boxplot(data=data, x="lr", y=measure, ax=axes[2])
+    sns.stripplot(data=data, x="lr", y=measure, ax=axes[2], color="black", alpha=0.5, jitter=True)
+    axes[2].set_title(f"{measure} against leaky rate")
+
+    plt.tight_layout()
+    plt.savefig(f"plots/{measure}_vs_parameters")
     plt.show()
+
+    # sns.pairplot(data, x_vars=["N", "sr", "lr"], y_vars=measure, kind="reg", height=4)
+    # plt.savefig(f"plots/{measure}_vs_parameters_v2")
+    # plt.show()
 
 def plot_correlation(data, param1, param2, measure):
     x = data[param1] + np.random.normal(0, 0.04*data[param1].std(), size=len(data))  # jitter
@@ -56,11 +56,21 @@ def get_parameters_under_threshold(threshold):
     return params
 
 if __name__ == "__main__":
-    data = pd.read_csv("results_parameters_ip_10_iter.csv")
+    data = pd.read_csv("results_parameters_ip_100_iter.csv")
+    plot = False
 
-    measure = "entropy_std"
-    # plot_parameters_against_measure(measure)
-    # plot_correlation(data, "lr", "sr", "KL_mean")
+    if plot:
+        # Plot parameters vs means and stds
+        measures = {"KL_mean", "entropy_mean", "KL_std", "entropy_std"}
+        for measure in measures:
+            plot_parameters_against_measure(measure)
+
+        # Plot correlations between parameters
+        params = {"N", "lr", "sr"}
+        for param1 in params:
+            for param2 in params:
+                if param1 != param2:
+                    plot_correlation(data, param1, param2, "KL_mean")
 
     # best_params = get_n_best_parameters(50)
     best_params = get_parameters_under_threshold(0.1)
@@ -76,7 +86,8 @@ if __name__ == "__main__":
 
     # plt.figure(figsize=(8, 6))
     # plt.scatter(data['entropy_mean'], data['entropy_std'])
-    # plt.xlabel("Entropy mean")
-    # plt.ylabel("Entropy std")
-    # plt.title("Entropy mean vs entropy std")
+    # plt.xlabel("entropy_std mean")
+    # plt.ylabel("entropy_std std")
+    # plt.title("entropy_std mean vs entropy_std")
+    # plt.savefig("plots/entropy_mean_vs_entropy_std")
     # plt.show()
